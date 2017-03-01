ef08541a9016075e1dfad7e00c4c4079c14ea937
Catherine Dornfeld <catherinedornfeld@Catherines-MacBook-Pro.local>
2017-03-01 11:12:00 -0600

    Add initial files

---
+++ logdata_Markov.py
@@ -0,0 +1,44 @@
+## THIS CODE IS MODIFIED FROM SAMPLE CODE GIVEN IN
+## DR. MATTHEW BERLAND'S COMPUTATIONAL RESEARCH METHODS COURSE (UW-MADISON)
+
+from collections import Counter
+import networkx as nx
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import csv
+
+###OPEN CSV AND GET MARKOV SEQUENCE
+csv_1 = csv.reader(open('/Users/catherinedornfeld/Desktop/CSCL_2017/LogData/Log_Walleye_LastSession.csv', 'rU'))
+myList1 = []
+next(csv_1) #skips header
+for row in csv_1:
+    myList1.append(row[0])
+print myList1
+
+###TRANSITION TABLES
+observations = myList1  #[1,2,3,2,1,2,3,1,3,3,1,1,2] #dumb sample data
+bigrams = zip(observations, observations[1:])
+outlinks = Counter()
+counts = Counter()
+for elt in bigrams:
+    counts[elt] += 1
+    outlinks[elt[0]] += 1
+mm = {elt: round(counts[elt]/float(outlinks[elt[0]]),2) for elt in counts}
+print mm
+
+###VISUALS
+G = nx.DiGraph()
+for edge in mm:
+    G.add_edge(edge[0],edge[1],label=mm[edge])
+
+##PROBLEM PIECE - MAY NEED TO TROUBLESHOOT NETWORKX
+A=nx.nx_agraph.to_agraph(G)
+A.graph_attr.update(dpi="600")
+A.layout(prog="dot")
+A.draw('logdata_Walleye.png') #change file name
+#A.draw('/Users/catherinedornfeld/Desktop/All_Disc.svg') # -> need to change below, too
+
+img=mpimg.imread('logdata_Walleye.png') #change file name
+imgplot = plt.imshow(img)
+plt.show()
+
